ddcfe44d7be7beecfc553dd42826ff6cdbd21ca7
Caroline Liongosari <CarolineLiong@comcast.net>
2018-06-15 15:41:34 -0700

    reducers and containers

--- ReduxCasts-master/book_list/src/reducers/index.js
+++ ReduxCasts-master/book_list/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import BooksReducer from "./reducer_books";
 import ActiveBook from "./reducer_active_book";
 
+//mapping of our state: BooksReducer is the array of books from reducer_books.js
 const rootReducer = combineReducers({
   books: BooksReducer,
   activeBook: ActiveBook
--- ReduxCasts-master/book_list/src/reducers/reducer_books.js
+++ ReduxCasts-master/book_list/src/reducers/reducer_books.js
@@ -1,3 +1,5 @@
+//just returns an array of books which a piece of our app's state
+//when importing this function in another file, it gives an array right away
 export default function() {
   return [
     { title: "Javascript: The Good Parts", pages: 101 },
